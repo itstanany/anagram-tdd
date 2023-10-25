04a7e1e0d7eb4507d48d1cb81764f3e41a3f337c
Tanany365 <ahmdgeek@gmail.com>
2023-10-25 20:21:04 +0300

    Anagrams - Working version

--- src/test/kotlin/AnagramTests.kt
+++ src/test/kotlin/AnagramTests.kt
@@ -1,51 +1,175 @@
 import org.junit.jupiter.api.Test
 import java.io.File
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 // static, only run once as we load the class
 val words = File("./scrabble.txt")
     .readLines()
+    .plus(
+        listOf(
+            "A",
+            "O",
+            "I",
+        )
+    )
+    .sorted()
+
 class AnagramTests {
     @Test
     fun `load words`() {
         assertEquals(
-            178691,
+            178694,
             words.size
         )
     }
 
-    // playing with a single word
+    // multi-word anagram
+    // filter potential words that can be made from the subject
+    // filter multi-word anagram that mirror the subject
+    
     @Test
-    fun `anagrms of cat`() {
-        val input = "CAT"
-        val anagrams = words
-            .filter {
-                it.length == 3
-            }
-            .filter {
-                it.hasSameLettersAs(input)
-            }
+    fun `can be made from the letters in`() {
 
+        assertTrue(
+            "A"
+                .couldBeMadeFromLettersIn(
+                    "A CAT"
+                )
+        )
+
+
+        assertTrue(
+            "AA"
+                .couldBeMadeFromLettersIn(
+                    "A CAT"
+                )
+        )
+        assertTrue(
+            "ACT"
+                .couldBeMadeFromLettersIn(
+                    "A CAT"
+                )
+        )
+        assertTrue(
+            "CAT"
+                .couldBeMadeFromLettersIn(
+                    "A CAT"
+                )
+        )
+
+
+        assertFalse(
+            "H"
+                .couldBeMadeFromLettersIn(
+                    "A CAT"
+                )
+        )
+        assertFalse(
+            "AAH"
+                .couldBeMadeFromLettersIn(
+                    "A CAT"
+                )
+        )
+        assertFalse(
+            "TAT"
+                .couldBeMadeFromLettersIn(
+                    "A CAT"
+                )
+        )
+
+        // boundary conditions
+        assertTrue(
+            ""
+                .couldBeMadeFromLettersIn(
+                    "A CAT"
+                )
+        )
+        assertTrue(
+            ""
+                .couldBeMadeFromLettersIn(
+                    ""
+                )
+        )
+    }
+
+    @Test
+    fun anagramsFor() {
+        val input = "A CAT"
         assertEquals(
-            listOf(
-                "ACT",
-                "CAT",
+            setOf(
+                "A ACT",
+                "A CAT",
+                "ACTA",
             ),
-            anagrams,
+            words.anagramsFor(input),
         )
     }
 }
 
-private fun String.hasSameLettersAs(
-    other: String,
-): Boolean {
-    return this
-        .toCharArray()
-        .sortedArray()
-        .contentEquals(
-            other
-            .toCharArray()
-            .sortedArray()
-        )
+fun List<String>.anagramsFor(input: String): Set<String> {
+    val result: MutableList<String> = mutableListOf<String>()
+    process(
+        input,
+        this,
+        { result.add(it) },
+    )
+    val deDuplicated: Set<String> = result.map { it.split(" ").sorted().joinToString(" ") }.toSet()
+    return deDuplicated
+}
+
+private fun process(
+    input: String,
+    words: List<String>,
+    collector: (String) -> Unit,
+    prefix: String = ""
+    ) {
+    // get all candidates
+    val _words = words.filter {
+        it.couldBeMadeFromLettersIn(input)
+    }
+//        println("_words ${_words.toString()}")
+            _words
+        .forEach { word ->
+            val remainingLetters = input.minusLettersIn(word)
+            if (remainingLetters.isNotBlank()) {
+//                println("word: $word,, remaining letters: [$remainingLetters]")
+//                println("$prefix $word [$remainingLetters]")
+
+                process(
+                    remainingLetters,
+                    words,
+                    collector,
+                    prefix = "  $word",
+                    )
+            } else {
+                collector("$prefix $word".trim())
+//                println("$prefix $word")
+//                println("word--no-remaining-letters: $word")
+
+                /* else {
+                                               println("word--no-remaining-letters: $word")
+                                           }*/
+            }
+        }
+}
+
+private fun String.minusLettersIn(word: String): String {
+    val lettersList = this.toMutableList()
+    word.forEach { char ->
+        if (!lettersList.remove(char)) error("BAD")
+    }
+    return String(lettersList.toCharArray())
+}
+
+private fun String.couldBeMadeFromLettersIn(letters: String): Boolean {
+    val lettersList = letters.toMutableList()
+// test presence of letters by looping
+//    removing chars guarantee testing letter repetition
+    this.forEach { char ->
+        if(!lettersList.remove(char)) return false
+    }
+    return true
 
 }
